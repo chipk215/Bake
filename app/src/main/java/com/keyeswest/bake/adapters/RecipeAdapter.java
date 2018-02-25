0f6e8951eb47cd2263ce7779536f8c44fed88bab
Chip Keyes <chipk215@msn.com>
2018-02-25 14:25:14 -0700

    Begin work on recipe list

---
+++ app/src/main/java/com/keyeswest/bake/adapters/RecipeAdapter.java
@@ -0,0 +1,54 @@
+package com.keyeswest.bake.adapters;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.keyeswest.bake.R;
+import com.keyeswest.bake.models.Recipe;
+
+import java.util.List;
+
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeHolder> {
+
+    private final List<Recipe> mRecipes;
+
+    public RecipeAdapter(List<Recipe> recipes){
+        mRecipes = recipes;
+    }
+
+    @Override
+    public RecipeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recipe_card, parent, false);
+
+        return new RecipeHolder(itemView);
+    }
+
+
+    @Override
+    public void onBindViewHolder(RecipeHolder holder, int position) {
+        Recipe recipe = mRecipes.get(position);
+        holder.name.setText(recipe.getName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRecipes.size();
+    }
+
+    static class RecipeHolder extends RecyclerView.ViewHolder{
+        public TextView name;
+
+        public RecipeHolder(View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.card_name_tv);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/keyeswest/bake/fragments/MasterListFragment.java
@@ -0,0 +1,62 @@
+package com.keyeswest.bake.fragments;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.keyeswest.bake.R;
+import com.keyeswest.bake.adapters.RecipeAdapter;
+import com.keyeswest.bake.models.Recipe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MasterListFragment extends Fragment {
+
+    private RecyclerView mRecipeRecyclerView;
+
+    private RecipeAdapter mRecipeAdapter;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        final View rootView = inflater.inflate(R.layout.fragment_master_list,
+                container, false);
+
+
+        mRecipeRecyclerView = rootView.findViewById(R.id.recipe_recycler_view);
+        mRecipeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+
+        List<Recipe> recipes = new ArrayList<>();
+        Recipe r1 = new Recipe();
+        r1.setName("Recipe 1");
+        recipes.add(r1);
+
+        Recipe r2 = new Recipe();
+        r2.setName("Recipe 2");
+        recipes.add(r2);
+
+
+
+        if (isAdded()){
+            mRecipeRecyclerView.setAdapter(new RecipeAdapter(recipes));
+        }
+
+        return rootView;
+    }
+}
