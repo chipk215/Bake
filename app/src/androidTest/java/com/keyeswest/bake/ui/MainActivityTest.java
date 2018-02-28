99d1e128e247f2cba3e35844f312e7fa3e0618f7
Chip Keyes <chipk215@msn.com>
2018-02-28 14:32:56 -0700

    Initial set of Espresso tests for Main and Detail Activities

--- app/src/androidTest/java/com/keyeswest/bake/ui/MainActivityTest.java
+++ app/src/androidTest/java/com/keyeswest/bake/ui/MainActivityTest.java
@@ -1,11 +1,90 @@
 package com.keyeswest.bake.ui;
 
 
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+
+import com.keyeswest.bake.MainActivity;
+import com.keyeswest.bake.R;
+
+
+import junit.framework.Assert;
+
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.scrollToPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.keyeswest.bake.ui.utils.atPosition;
+import static com.keyeswest.bake.ui.utils.isTablet;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
+
+    private static final String NUTELLA = "Nutella Pie";
+    private static final String CHEESECAKE = "Cheesecake";
+    private static final String BROWNIES = "Brownies";
+
+
+    // Launch the MainActivity prior to each test
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+
+    @Test
+    public void launchTest(){
+        onView(withId(R.id.recipe_recycler_view)).check(matches(isDisplayed()));
+    }
+
+
+    /**
+     * Check the first and fourth items in the ist for correct names
+     */
+    @Test
+    public void recipeNameTest(){
+
+        // check the first item
+        onView(withId(R.id.recipe_recycler_view))
+                .check(matches(atPosition(0,hasDescendant(withText(NUTELLA)))));
+
+        //check the 4th item
+        onView(withId(R.id.recipe_recycler_view))
+                .perform(scrollToPosition(3))
+                .check(matches(atPosition(3,hasDescendant(withText(CHEESECAKE)))));
+
+    }
+
+
+    @Test
+    public void clickRecipeItemOpensRecipeDetailActivityOnPhone(){
+
+       if (! isTablet(InstrumentationRegistry.getTargetContext())) {
+
+           onView(withId(R.id.recipe_recycler_view))
+                   .perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
+
+           onView(withId(R.id.recipe_name_tv)).check(matches(isDisplayed()));
+
+           onView(withId(R.id.recipe_name_tv)).check(matches(withText(BROWNIES)));
+       }
+
+        Assert.assertTrue(true);
+
+    }
+
 
 }
---
+++ app/src/androidTest/java/com/keyeswest/bake/ui/utils.java
@@ -0,0 +1,50 @@
+package com.keyeswest.bake.ui;
+
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.support.annotation.NonNull;
+import android.support.test.espresso.matcher.BoundedMatcher;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+
+
+
+import static android.support.test.espresso.core.internal.deps.guava.base.Preconditions.checkNotNull;
+
+public class utils {
+
+
+    // Attribution https://stackoverflow.com/a/34795431/9128441
+    public static Matcher<View> atPosition(final int position, @NonNull final Matcher<View> itemMatcher) {
+        checkNotNull(itemMatcher);
+        return new BoundedMatcher<View, RecyclerView>(RecyclerView.class) {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("has item at position " + position + ": ");
+                itemMatcher.describeTo(description);
+            }
+
+            @Override
+            protected boolean matchesSafely(final RecyclerView view) {
+                RecyclerView.ViewHolder viewHolder = view.findViewHolderForAdapterPosition(position);
+                if (viewHolder == null) {
+                    // has no item on such position
+                    return false;
+                }
+                return itemMatcher.matches(viewHolder.itemView);
+            }
+        };
+    }
+
+
+    // Attribution https://stackoverflow.com/a/11330947/9128441
+    public static boolean isTablet(Context context) {
+        return (context.getResources().getConfiguration().screenLayout
+                & Configuration.SCREENLAYOUT_SIZE_MASK)
+                >= Configuration.SCREENLAYOUT_SIZE_LARGE;
+    }
+}
