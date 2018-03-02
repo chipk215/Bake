17392348bd7007168da576c65b21e54a1ebf2ab5
Chip Keyes <chipk215@msn.com>
2018-03-02 11:24:59 -0700

    Initial tablet layout, phone scenario still works

--- app/src/main/java/com/keyeswest/bake/MainActivity.java
+++ app/src/main/java/com/keyeswest/bake/MainActivity.java
@@ -2,21 +2,42 @@ package com.keyeswest.bake;
 
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.keyeswest.bake.fragments.MasterListFragment;
 import com.keyeswest.bake.models.Recipe;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class MainActivity extends AppCompatActivity implements MasterListFragment.OnRecipeSelected {
     private static final String TAG = "MainActivity";
 
+    private boolean mTwoPane;
+
+    @Nullable
+    @BindView(R.id.divider_view)  View mTwoPaneDivider;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        if (mTwoPaneDivider != null){
+            // handle split screen
+            mTwoPane = true;
+        }else{
+            mTwoPane = false;
+        }
+
     }
 
 
@@ -36,8 +57,14 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
             Log.e(TAG, "Error, no recipe returned");
         }
 
-        Intent intent = RecipeDetailActivity.newIntent(this, recipe);
-        startActivity(intent);
+        if (mTwoPane){
+            // wide screen
+
+        }else{
+            Intent intent = RecipeDetailActivity.newIntent(this, recipe);
+            startActivity(intent);
+        }
+
 
     }
 }
