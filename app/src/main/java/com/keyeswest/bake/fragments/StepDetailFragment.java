dc79d61b42bf248d82278d98bd8bc457f75a87e3
Chip Keyes <chipk215@msn.com>
2018-03-06 13:54:37 -0700

    Initial implementation of media player

--- app/src/main/java/com/keyeswest/bake/fragments/StepDetailFragment.java
+++ app/src/main/java/com/keyeswest/bake/fragments/StepDetailFragment.java
@@ -1,7 +1,9 @@
 package com.keyeswest.bake.fragments;
 
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,6 +14,16 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.keyeswest.bake.R;
 import com.keyeswest.bake.models.Step;
 
@@ -52,6 +64,12 @@ public class StepDetailFragment  extends Fragment {
     @BindView(R.id.step_description_tv)TextView mDescriptionTextView;
     @BindView(R.id.prev_button)Button mPreviousButton;
     @BindView(R.id.next_button)Button mNextButton;
+    @BindView(R.id.video_view) SimpleExoPlayerView playerView;
+
+    private int mCurrentWindow =0;
+    private long mPlaybackPosition =0;
+    private SimpleExoPlayer mPlayer;
+    private boolean mPlayWhenReady = true;
 
     @Nullable
     @BindView(R.id.step_complete_cb)CheckBox mStepCheckBox;
@@ -131,11 +149,99 @@ public class StepDetailFragment  extends Fragment {
         return view;
     }
 
+
+    //=== Media Player Code Attribution
+    //    https://codelabs.developers.google.com/codelabs/exoplayer-intro/#2
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+        }
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideSystemUi();
+        if ((Util.SDK_INT <= 23 || mPlayer == null)) {
+            initializePlayer();
+        }
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
+    }
+
     @Override
     public void onDestroyView(){
         super.onDestroyView();
         mUnbinder.unbind();
     }
+
+
+
+    private void initializePlayer(){
+        mPlayer = ExoPlayerFactory.newSimpleInstance(
+                new DefaultRenderersFactory(getContext()),
+                new DefaultTrackSelector(), new DefaultLoadControl());
+
+        playerView.setPlayer(mPlayer);
+
+        mPlayer.setPlayWhenReady(mPlayWhenReady);
+        mPlayer.seekTo(mCurrentWindow, mPlaybackPosition);
+
+        if (mStep.getVideoURL() != null){
+            Uri uri = Uri.parse(mStep.getVideoURL());
+            MediaSource mediaSource = buildMediaSource(uri);
+            mPlayer.prepare(mediaSource, true, false);
+        }
+    }
+
+    private MediaSource buildMediaSource(Uri uri){
+        return new ExtractorMediaSource.Factory(
+                new DefaultHttpDataSourceFactory("exoplayer-keyeswest-bake"))
+                .createMediaSource(uri);
+
+    }
+
+
+    @SuppressLint("InlinedApi")
+    private void hideSystemUi() {
+        playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+
+    private void releasePlayer() {
+        if (mPlayer != null) {
+            mPlaybackPosition = mPlayer.getCurrentPosition();
+            mCurrentWindow = mPlayer.getCurrentWindowIndex();
+            mPlayWhenReady = mPlayer.getPlayWhenReady();
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
 
 
 }
