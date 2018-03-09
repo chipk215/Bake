45b9206e1952c6209b458f5780b8b4f658507d46
Chip Keyes <chipk215@msn.com>
2018-03-09 15:01:52 -0700

    Toasting item views

--- app/src/main/java/com/keyeswest/bake/BakeAppWidget.java
+++ app/src/main/java/com/keyeswest/bake/BakeAppWidget.java
@@ -1,5 +1,6 @@
 package com.keyeswest.bake;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
@@ -8,6 +9,7 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.util.Log;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import com.keyeswest.bake.utilities.RecipeWriter;
 
@@ -22,6 +24,8 @@ import java.util.List;
 public class BakeAppWidget extends AppWidgetProvider {
 
     private static final String TAG="BAKEAPPWIDGET";
+
+    public static final String TOAST_ACTION = "com.keyeswest.bake.BakeAppWidget.TOAST_ACTION";
     public static final String EXTRA_ITEM = "com.keyeswest.bake.BakeAppWidget.EXTRA_ITEM";
 
 
@@ -41,6 +45,26 @@ public class BakeAppWidget extends AppWidgetProvider {
         views.setEmptyView(R.id.recipe_list, R.id.empty_view);
 
 
+
+        // This section makes it possible for items to have individualized behavior.
+        // It does this by setting up a pending intent template. Individuals items of a collection
+        // cannot set up their own pending intents. Instead, the collection as a whole sets
+        // up a pending intent template, and the individual items set a fillInIntent
+        // to create unique behavior on an item-by-item basis.
+        Intent toastIntent = new Intent(context, BakeAppWidget.class);
+        // Set the action for the intent.
+        // When the user touches a particular view, it will have the effect of
+        // broadcasting TOAST_ACTION.
+        toastIntent.setAction(BakeAppWidget.TOAST_ACTION);
+        toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+        PendingIntent toastPendingIntent = PendingIntent.getBroadcast(context, 0, toastIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        views.setPendingIntentTemplate(R.id.recipe_list, toastPendingIntent);
+
+
+
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
@@ -53,7 +77,22 @@ public class BakeAppWidget extends AppWidgetProvider {
         }
 
         super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+
     }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        AppWidgetManager mgr = AppWidgetManager.getInstance(context);
+        if (intent.getAction().equals(TOAST_ACTION)) {
+            int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                    AppWidgetManager.INVALID_APPWIDGET_ID);
+            int viewIndex = intent.getIntExtra(EXTRA_ITEM, 0);
+            Toast.makeText(context, "Touched view " + viewIndex, Toast.LENGTH_SHORT).show();
+        }
+        super.onReceive(context, intent);
+    }
+
 
 
 }
